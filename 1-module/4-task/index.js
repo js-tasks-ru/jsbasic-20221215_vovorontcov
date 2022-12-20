17bfb16c78f5296647afaeecb336a62a1e9673dd
Vlad Vorontsov <vovorontcov@icloud.com>
2022-12-20 08:17:22 +0300

    Чек спама

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,10 @@
 function checkSpam(str) {
-  // ваш код...
+  let result;
+  str = str.toLowerCase();
+  if (str.includes('1xbet')) {
+    result = true;
+  } else if (str.includes('xxx')) {
+    result = true;
+  } else result = false;
+  return result;
 }
