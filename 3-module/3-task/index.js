5322c25d4b82cd0ad6c845f81ce0cf08a93097a6
Vladislav Vorontsov <vladislav.vorontsov@tura-group.ru>
2023-01-10 06:51:33 +0300

    Camelization

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,10 @@
 function camelize(str) {
-  // ваш код...
+  let arr = str.split('-');
+  let result = [];
+
+  arr.forEach((item, index) => {
+    if (index == 0) result.push(item)
+      else result.push(item[0].toUpperCase()+item.slice(1))
+  });
+  return result = result.join('');
 }
