ffb5cb3b2e824863854e228a38519cefa33c723f
Vladislav Vorontsov <vladislav.vorontsov@tura-group.ru>
2023-01-10 08:12:30 +0300

    min & max

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,17 @@
 function getMinMax(str) {
-  // ваш код...
+  let arr = str.split(' ');
+  let result = [];
+
+  arr.forEach((item) => {
+    if (Number(item) !== Number(item)) 0
+      else result.push(Number(item));
+  });
+
+  let min = Math.min(...result);
+  let max = Math.max(...result);
+
+  return result = {
+    min: min,
+    max: max,
+  };
 }
